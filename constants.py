368e3d448b76b3f84222c97b9d6eef667081edba
Pullusb <bernou.samuel@gmail.com>
2026-07-12 15:25:51 +0200

    new constants to replace default layer and material stacks (replicate materials from json)

--- constants.py
+++ constants.py
@@ -2,6 +2,30 @@ from pathlib import Path
 
 LAYERMAT_PREFIX = 'mat--'
 
+## Default layer stack for new GP objects: (layer name + default associated material)
+DEFAULT_LAYER_STACK = (
+    ('Annotate', 'line_red'),
+    ('Sketch', 'line'),
+    ('Line', 'line'),
+    ('Color', 'fill_white'),
+)
+
+## Default material stack for new GP objects
+## same_color bool: use the stroke color for both stroke and fill
+## holdout bool: Activate holdout for this material
+DEFAULT_MATERIAL_STACK = (
+    {'name': 'line', 'stroke_color': (0.0, 0.0, 0.0, 1.0), 'fill_color': (0.0, 0.0, 0.0, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'line_white', 'stroke_color': (1.0, 1.0, 1.0, 1.0), 'fill_color': (1.0, 1.0, 1.0, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'fill_white', 'stroke_color': (1.0, 1.0, 1.0, 1.0), 'fill_color': (1.0, 1.0, 1.0, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'fill_grey_light', 'stroke_color': (0.630757, 0.630757, 0.630757, 1.0), 'fill_color': (0.630757, 0.630757, 0.630757, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'fill_grey_mid', 'stroke_color': (0.361307, 0.361307, 0.361307, 1.0), 'fill_color': (0.361307, 0.361307, 0.361307, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'fill_grey_dark', 'stroke_color': (0.102242, 0.102242, 0.102242, 1.0), 'fill_color': (0.102242, 0.102242, 0.102242, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'fill_black', 'stroke_color': (0.0, 0.0, 0.0, 1.0), 'fill_color': (0.0, 0.0, 0.0, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'line_red', 'stroke_color': (0.7, 0.0, 0.0, 1.0), 'fill_color': (0.7, 0.0, 0.0, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'line_blue', 'stroke_color': (0.051, 0.646, 1.0, 1.0), 'fill_color': (0.051, 0.646, 1.0, 1.0), 'same_color': True, 'holdout': False},
+    {'name': 'fill_mask', 'stroke_color': (0.0, 0.0, 0.0, 1.0), 'fill_color': (0.0, 0.0, 0.0, 1.0), 'same_color': True, 'holdout': True},
+)
+
 MODULE_DIR = Path(__file__).parent
 
 RESOURCES_DIR = MODULE_DIR / 'resources'
