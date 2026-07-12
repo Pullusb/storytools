0178b28aad0be7f5cb0988efc92f94a78e9012e1
Pullusb <bernou.samuel@gmail.com>
2026-07-12 17:25:56 +0200

    remove old palette loader

--- fn.py
+++ fn.py
@@ -969,37 +969,6 @@ def name_to_hue(name):
     # Convert first 8 characters of hash to integer and scale to 0-1
     return int(hash_value[:8], 16) / 0xffffffff
 
-def load_palette(filepath, ob=None):
-    with open(filepath, 'r') as fd:
-        mat_dic = json.load(fd)
-    # from pprint import pprint
-    # pprint(mat_dic)
-    
-    if ob is None:
-        ob = bpy.context.object
-
-    for mat_name, attrs in mat_dic.items():
-        curmat = bpy.data.materials.get(mat_name)
-        if curmat:
-            # exists
-            if curmat.is_grease_pencil:
-                if curmat not in ob.data.materials[:]: # add only if it's not already there
-                    ob.data.materials.append(curmat)
-                continue
-            else:
-                mat_name = mat_name + '.01' # rename to avoid conflict
-
-        ## Create a GP mat
-        mat = bpy.data.materials.new(name=mat_name)
-        bpy.data.materials.create_gpencil_data(mat)
-        
-        ob.data.materials.append(mat)
-        for attr, value in attrs.items():
-            if bpy.app.version >= (5, 1, 0) and attr in ('show_fill', 'show_stroke'):
-                ## Starting Blender 5.1, Those are transferred to stroke and are now deprecated.
-                continue
-            setattr(mat.grease_pencil, attr, value)
-
 
 def get_default_material_stack_entries(prefs=None):
     """Return the default material stack as a list of dicts
